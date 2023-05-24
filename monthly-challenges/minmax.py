5b281c9e1542ffdb6c98a658a6d685c13d422eba
imjamnotjelly <81252683+imjamnotjelly@users.noreply.github.com>
2023-05-24 14:57:36 -0400

    Update minmax.py

--- monthly-challenges/minmax.py
+++ monthly-challenges/minmax.py
@@ -3,17 +3,17 @@ from math import sqrt
 def ab_v(x):
     return sqrt(x**2)
     
-def ab_s(x, y):
+def ab_d(x, y):
     return ab_v(x - y)
     
 def mean(x, y):
     return (x + y)/2
 
 def minimum(x, y):
-    return mean(x, y) - ab_s(x, y)/2
+    return mean(x, y) - ab_d(x, y)/2
 
 def maximum(x, y):
-    return mean(x, y) + ab_s(x, y)/2
+    return mean(x, y) + ab_d(x, y)/2
 
 test = [8, 4]
 
