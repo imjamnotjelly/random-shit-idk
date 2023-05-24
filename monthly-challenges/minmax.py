7e579f72b7ae81d2180c323fe26c8efd737df0f4
imjamnotjelly <81252683+imjamnotjelly@users.noreply.github.com>
2023-05-24 14:50:42 -0400

    Create minmax.py

---
+++ monthly-challenges/minmax.py
@@ -0,0 +1,22 @@
+from math import sqrt
+
+def ab_v(x):
+    return sqrt(x**2)
+    
+def ab_s(x, y):
+    return ab_v(x - y)
+    
+def mean(x, y):
+    return (x + y)/2
+
+def minimum(x, y):
+    return mean(x, y) - ab_s(x, y)/2
+
+def maximum(x, y):
+    return mean(x, y) + ab_s(x, y)/2
+
+test = [8, 4]
+
+print(minimum(*test))
+
+print(maximum(*test))
