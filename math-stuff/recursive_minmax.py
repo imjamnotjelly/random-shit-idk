6c56a671d15f3f2bcbf2d6edded6a75cfe1ab897
imjamnotjelly <81252683+imjamnotjelly@users.noreply.github.com>
2023-05-30 11:21:32 -0400

    Create recursive_minmax.py

---
+++ math-stuff/recursive_minmax.py
@@ -0,0 +1,27 @@
+# recursive min/max func generator (very lag!!!)
+
+from math import sqrt
+
+def minimum(*nums):
+    if len(nums) > 2:
+        x = minimum(*nums[:-1])
+        y = nums[-1]
+    else:
+        x, y = nums
+    return f"({x}+{y}-sqrt(({x}-{y})**2))/2"
+
+def maximum(*nums):
+    if len(nums) > 2:
+        x = minimum(*nums[:-1])
+        y = nums[-1]
+    else:
+        x, y = nums
+    return f"({x}+{y}+sqrt(({x}-{y})**2))/2"
+
+nrange = lambda x:tuple(range(1, x+1))
+test = nrange(20)
+
+min_func = minimum(*test)
+max_func = maximum(*test)
+print(min_func, eval(min_func))
+print(max_func, eval(max_func))
