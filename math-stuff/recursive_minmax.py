99b36fde573c248530cecfcb700698abf65ff48c
imjamnotjelly <jonahmitre@gmail.com>
2023-06-17 10:31:54 -0400

    iterative min max variant

--- math-stuff/recursive_minmax.py
+++ math-stuff/recursive_minmax.py
@@ -1,8 +1,9 @@
-# recursive min/max func generator (very lag!!!)
-
 from math import sqrt
 from random import uniform
+from random import sample
+# recursive min/max (very lag!!)
 
+"""
 def minimum(*nums):
     if len(nums) > 2:
         x = minimum(*nums[:-1])
@@ -18,14 +19,28 @@ def maximum(*nums):
     else:
         x, y = nums
     return f"({x}+{y}+sqrt(({x}-{y})**2))/2" # yeah yeah im aware that theres no abstraction
+"""
 
-nrange = lambda x:tuple(range(1, x+1))
+# iterative min/max
+def minimum(*nums):
+    x = nums[0]
+    for y in nums[1:]:
+        x = f"({x}+{y}-sqrt(({x}-{y})**2))/2"
+    return x
+
+def maximum(*nums):
+    x = nums[0]
+    for y in nums[1:]:
+        x = f"({x}+{y}+sqrt(({x}-{y})**2))/2"
+    return x
+
+nrange = lambda x:sample(list(range(1, x+1)), x)
 rrange = lambda mn, mx, amt:tuple(uniform(mn, mx) for _ in range(amt))
 
-test = rrange(1, 10, 5)
+test = rrange(1, 100, 10)
 print(test)
 
 min_func = minimum(*test)
 max_func = maximum(*test)
-print(min_func, eval(min_func))
+print(min_func, eval(min_func), "\n")
 print(max_func, eval(max_func))
