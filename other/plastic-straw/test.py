4f68b76ecee5a8ad051c6c35d7d5bb46432963f4
imjamnotjelly <jonahmitre@gmail.com>
2023-06-20 18:25:57 -0400

    centering

--- other/plastic-straw/test.py
+++ other/plastic-straw/test.py
@@ -23,16 +23,22 @@ def index_cdn(iterable, condition, default=-1):
 def insert(string, index, char):
     return string[:index] + char + string[index:]
 
+def hide(func, *args):
+    pu(); func(*args); pd()
+
 def rect(width, height, radius=0):
     dim = (width, height)
+    cx, cy = pos()
     if isinstance(radius, str):
         l_index = index_cdn(radius, str.isalpha)
         radius = eval(insert(radius, l_index, "*"))
     radius = min(radius, min(dim)/2)
+    hide(goto, cx-width/2+radius, cy-height/2)
     for turn in range(4):
         fd(dim[turn%2]-radius*2)
         circle(radius, 90)
-pd()
-rect(300, 100, "1/8 min(dim)")
-pu()
+
+ht()
+speed(0)
+rect(100, 300, 10)
 done()
